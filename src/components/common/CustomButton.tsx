ec3df61f4b18bb6eb617b76e768b763bd9ad2a00
Deepak Verma <deepakradialcode@gmail.com>
2025-02-13 10:29:01 +0530

    add hover on button

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -9,7 +9,7 @@ const CustomButton = ({ myClass, text, customOnClick }: CustomButtonProps) => {
   return (
     <button
       onClick={customOnClick}
-      className={`${myClass} rounded-lg tracking-[0.87px] py-[11px] px-[25.07px] shadow-[inset_0px_4px_4px_0px] shadow-lightOrange bg-gradient-to-b from-purple via-pink to-orange font-extrabold text-white text-xl max-sm:text-base max-sm:leading-[19.2px] leading-6`}
+      className={`${myClass} rounded-lg tracking-[0.87px] py-[11px] px-[25.07px] shadow-[inset_0px_4px_4px_0px] hover:scale-95 transition-all duration-300 shadow-lightOrange bg-gradient-to-b from-purple via-pink to-orange font-extrabold text-white text-xl max-sm:text-base max-sm:leading-[19.2px] leading-6`}
     >
       {text}
     </button>
